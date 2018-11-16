aa51f36854340e3d49f5572f592b0079416e190a
XiaoWei <70285622@qq.com>
2018-11-16 13:19:36 +0800

    update config

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+cat /etc/sysctl.conf | grep -v "vm.max_map_count" > /tmp/system_sysctl.conf
+
+echo "vm.max_map_count=262144" >> /tmp/system_sysctl.conf
+
+mv /tmp/system_sysctl.conf /etc/sysctl.conf
+
+sysctl -w vm.max_map_count=262144
+
+sysctl -a|grep vm.max_map_count
+
+mkdir -p elasticsearch
+
+echo "==== build successed! ===="
